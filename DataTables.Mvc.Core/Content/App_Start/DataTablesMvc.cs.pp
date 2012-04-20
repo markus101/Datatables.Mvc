573fad6ae18ad90e3b1f0ea02debad2d41a93e5e
Mark McDowall <markus.mcd5@gmail.com>
2012-04-20 14:26:21 -0700

    Better NUGET packaging.

---
+++ DataTables.Mvc.Core/Content/App_Start/DataTablesMvc.cs.pp
@@ -0,0 +1,21 @@
+﻿﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq;
+using Microsoft.Web.Infrastructure;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+[assembly: WebActivator.PreApplicationStartMethod(
+typeof($rootnamespace$.App_Start.DataTablesMvcPackage), "PreStart")]
+
+namespace $rootnamespace$.App_Start
+{
+    public static class DataTablesMvcPackage
+    {
+        public static void PreStart()
+        {
+            if (!ModelBinders.Binders.ContainsKey(typeof(DataTablesParams)))
+                ModelBinders.Binders.Add(typeof(DataTablesParams), new DataTablesModelBinder());
+        }
+    }
+}
