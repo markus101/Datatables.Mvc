4bedea772e3f382793b9ed522abbe4db9b4fc36a
Mark McDowall <markus.mcd5@gmail.com>
2012-04-20 14:48:35 -0700

    Fixed nusepc and install files.

--- DataTables.Mvc.Core/tools/install.ps1
+++ DataTables.Mvc.Core/tools/install.ps1
@@ -2,5 +2,4 @@
 
 $path = [System.IO.Path]
 $appstart = $path::Combine($path::GetDirectoryName($project.FileName), "App_Start\DataTablesMvc.cs")
-$models = $path::Combine($path::GetDirectoryName($project.FileName), "Models\DataTablesParams.cs")
 $DTE.ItemOperations.OpenFile($appstart)
